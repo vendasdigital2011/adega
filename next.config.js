d732d915af9ab960ab5da2dc60b4bae0ccd09575
vendasdigital2011 <vendasdigital2011@gmail.com>
2026-07-29 16:17:31 -0300

    fix: adicionar unload=(self *) no Permissions-Policy em next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const securityHeaders = [
   { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
   {
     key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), payment=(), usb=()",
+    value: "camera=(), microphone=(), geolocation=(), payment=(), usb=(), unload=(self \"*\")",
   },
   { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
 ]
